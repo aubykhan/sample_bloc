f29d9c2cc1abe7049586cf9092d8608c6ef00cc5
aubykhan <aubykhan@gmail.com>
2018-05-01 22:51:42 +0500

    Everything's working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'GitHub Job Search'),
     );
   }
 }
@@ -37,8 +39,20 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Column(
         children: <Widget>[
-          TextField(onChanged: bloc.query.add),
-          _buildResults(),
+          new Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 8.0,
+            ),
+            child: TextField(
+              onChanged: bloc.query.add,
+              decoration: InputDecoration(
+                icon: Icon(Icons.search),
+                hintText: 'Search job',
+              ),
+            ),
+          ),
+          new Flexible(child: _buildResults()),
         ],
       ),
     );
@@ -47,18 +61,38 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget _buildResults() {
     return StreamBuilder(
       stream: bloc.results,
-      builder: (_, snapshot) => ListView.builder(
-            itemBuilder: (_, index) => ListTile(
-                  title: Text(snapshot?.data[index]),
-                ),
-          ),
+      builder: (_, snapshot) =>
+          (snapshot?.connectionState == ConnectionState.waiting)
+              ? new Center(child: CircularProgressIndicator())
+              : (snapshot?.data as List).length > 0
+                  ? ListView.builder(
+                      itemBuilder: (_, index) => ListTile(
+                            title: Text(snapshot?.data[index]),
+                          ),
+                    )
+                  : new Center(child: Text('No data found')),
     );
   }
 }
 
 class Api {
+  final HttpClient _client = new HttpClient();
+  static const String _url =
+      'https://jobs.github.com/positions.json?description={0}';
+
   Future<List<String>> search(String query) async {
-    return ['hey'];
+    var request =
+        await _client.getUrl(Uri.parse(_url.replaceFirst('{0}', query)));
+    var response = await request.close();
+
+    List<String> list = await response
+        .transform(utf8.decoder)
+        .transform(json.decoder)
+        .expand((json) => json as List)
+        .map((json) => (json as Map)['title'] as String)
+        .toList();
+
+    return list;
   }
 }
 
@@ -71,7 +105,7 @@ class SearchBloc {
 
   //With RxDart
   //ReplaySubject<String> _query = new ReplaySubject<String>();
-  
+
   //With built-in streams
   StreamController<String> _query = new StreamController<String>();
   Sink<String> get query => _query;
